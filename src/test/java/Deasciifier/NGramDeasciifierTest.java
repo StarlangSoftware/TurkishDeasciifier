50bffbfceea1c65f1583482adf8debff0478ffb4
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-10-01 12:56:00 +0300

    Upgraded version.

--- src/test/java/Deasciifier/NGramDeasciifierTest.java
+++ src/test/java/Deasciifier/NGramDeasciifierTest.java
@@ -1,6 +1,5 @@
 package Deasciifier;
 
-import Corpus.Corpus;
 import Corpus.Sentence;
 import MorphologicalAnalysis.FsmMorphologicalAnalyzer;
 import Ngram.LaplaceSmoothing;
@@ -21,25 +20,6 @@ public class NGramDeasciifierTest {
         fsm = new FsmMorphologicalAnalyzer();
         nGram = new NGram<String>("ngram.txt");
         nGram.calculateNGramProbabilities(new NoSmoothing<>());
-    }
-
-    @Test
-    public void testDeasciify() {
-        NGramDeasciifier nGramDeasciifier = new NGramDeasciifier(fsm, nGram, true);
-        SimpleAsciifier simpleAsciifier = new SimpleAsciifier();
-        Corpus corpus = new Corpus("corpus.txt");
-        for (int i = 0; i < corpus.sentenceCount(); i++){
-            Sentence sentence = corpus.getSentence(i);
-            for (int j = 1; j < sentence.wordCount(); j++){
-                if (fsm.morphologicalAnalysis(sentence.getWord(j).getName()).size() > 0){
-                    String asciified = simpleAsciifier.asciify(sentence.getWord(j));
-                    if (!asciified.equals(sentence.getWord(j).getName())){
-                        Sentence deasciified = nGramDeasciifier.deasciify(new Sentence(sentence.getWord(j - 1).getName() + " " + sentence.getWord(j).getName()));
-                        assertEquals(sentence.getWord(j).getName(), deasciified.getWord(1).getName());
-                    }
-                }
-            }
-        }
     }
 
     @Test
