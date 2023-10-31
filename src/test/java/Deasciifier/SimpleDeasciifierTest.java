32d9e39e0e333b6d6eb84873f06d4fbe17c57e59
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-10-31 12:55:53 +0300

    Added new test for deasciifying.

--- src/test/java/Deasciifier/SimpleDeasciifierTest.java
+++ src/test/java/Deasciifier/SimpleDeasciifierTest.java
@@ -1,10 +1,19 @@
 package Deasciifier;
 
 import Corpus.Sentence;
+import Dictionary.Word;
 import MorphologicalAnalysis.FsmMorphologicalAnalyzer;
+import MorphologicalAnalysis.FsmParse;
+import MorphologicalAnalysis.FsmParseList;
 import Ngram.NGram;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -24,6 +33,31 @@ public class SimpleDeasciifierTest {
         assertEquals("hakkında", simpleDeasciifier.deasciify(new Sentence("hakkinda")).toString());
         assertEquals("küçük", simpleDeasciifier.deasciify(new Sentence("kucuk")).toString());
         assertEquals("karşılıklı", simpleDeasciifier.deasciify(new Sentence("karsilikli")).toString());
+    }
+
+    @Test
+    public void testDistinctWordList(){
+        SimpleDeasciifier simpleDeasciifier = new SimpleDeasciifier(fsm);
+        try {
+            PrintWriter output1 = new PrintWriter("deasciified.txt");
+            PrintWriter output2 = new PrintWriter("not-analyzed.txt");
+            Scanner input = new Scanner(new File("distinct.txt"));
+            while (input.hasNext()){
+                String word = input.next();
+                Sentence sentence = new Sentence(word);
+                Sentence newSentence = simpleDeasciifier.deasciify(sentence);
+                if (!sentence.toString().equals(newSentence.toString())){
+                    output1.println(word + "\t" + newSentence.toString());
+                } else {
+                    output2.println(word);
+                }
+            }
+            input.close();
+            output1.close();
+            output2.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
